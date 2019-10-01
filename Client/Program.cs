42836128ef776fc748a4089511e4cd23c78c9275
Anderson Pessoa <anderson.marques@sp.senai.br>
2019-10-01 17:48:41 -0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new AsynchronousClient();
+
+            client.StartClient();
+
+            client.Send("Message 1");
+
+            var response = client.Receive();
+
+            Console.WriteLine(response);
+
+            client.Send("Message 2");
+
+            var response2 = client.Receive();
+
+            Console.WriteLine(response2);
+
+            client.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var listener = new AsynchronousSocketListener();
+
+            listener.StartListening();
+        }
+    }
+}
