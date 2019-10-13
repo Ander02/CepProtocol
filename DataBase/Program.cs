07f7c31e75ee939569884ddc0166ff4e661327ec
mrblondjose <grupo.startup2016@gmail.com>
2019-10-13 01:45:30 -0300

    Ajuste no DbWrite. O Header do arquivo passou a ser um parametro. Essa classe pode gerenciar entao um arquivo de Header generico

--- DataBase/Program.cs
+++ DataBase/Program.cs
@@ -9,19 +9,21 @@ namespace DataBase
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            DBWriter DB = new DBWriter("C:\\Users\\Gu\\Desktop\\Gustavo\\USP\\6 semestre\\Redes\\EP\\CepProtocol\\DataBase\\Server_DB.txt");
-            String [] x= {"a","1"};
-            
-            DB.ShowBuffer();
+            String FilePath= "C:\\Users\\Gu\\Desktop\\Gustavo\\USP\\6 semestre\\Redes\\EP\\CepProtocol\\DataBase\\UsersTable.txt";
+            String Header= "UserID;UserName;Password;";
+            DBWriter DB = new DBWriter(FilePath,Header);
+            String [] x= {"user2","jonas","password"};
+            DB.insertLine(x);
+            //DB.ShowBuffer();
             Console.WriteLine(DB.GetColumIndex("Cep"));
-            List<List<String>> c= DB.GetLines("UserIP","b");
+            List<List<String>> c= DB.GetLines("UserId","user2");
             showList(c);
         }
 
         static void showList(List<List<String>> l){
             foreach (List<String> line in l){
                 foreach (String value in line){
-                    Console.Write(value);
+                    Console.Write(value+" ");
                 }                        
                 Console.WriteLine();
             }
